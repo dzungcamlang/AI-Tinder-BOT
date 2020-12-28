775080d8c9c26e93997986a2a196d58817062e74
Dzung Cam Lang <dungcamlang@gmail.com>
2020-12-28 21:11:59 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install chromium-chromedriver
+pip install selenium
+pip install webdriver_manager
+sudo apt-get install -y xvfb 
+pip install pyvirtualdisplay
+pip install 2captcha-python==1.0.3
+pip install random-user-agent==0.0.6
+pip install anticaptchaofficial
